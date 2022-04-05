f20d6d195cc5b844dabecdd1735f8ec110b94c13
michal7272 <michal7272@seznam.cz>
2022-04-06 00:06:38 +0200

    Added search filter

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
 baguetteBox.run('.gallery');
-const search = new Filter('search', 'data-caption');
+
+const searchFilter = () => {
+    let input = document.getElementById('search').value;
+    input = input.toLowerCase();
+    let links = document.querySelectorAll('a');
+    let captions = [];
+
+    for (let i = 0; i < links.length; i++) {
+        captions[i] = links[i].getAttribute("data-caption");
+
+        if (!captions[i].toLowerCase().includes(input)) {
+            links[i].style.display="none";  
+        }
+        else {
+            links[i].style.display="";               
+        }
+    }
+};
+
