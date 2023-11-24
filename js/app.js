7e0a65d5eba4d1b925c48c972a151d8569fbbca2
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-11-24 21:08:30 +0100

    Change indent to 2 spaces

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 baguetteBox.run(`.gallery`);
 
 const searchFilter = () => {
-    let input = document.getElementById(`search`).value;
-    input = input.toLowerCase();
-    let links = document.querySelectorAll(`a`);
-    let captions = [];
+  let input = document.getElementById(`search`).value;
+  input = input.toLowerCase();
+  let links = document.querySelectorAll(`a`);
+  let captions = [];
 
-    for (let i = 0; i < links.length; i++) {
-        captions[i] = links[i].getAttribute(`data-caption`);
+  for (let i = 0; i < links.length; i++) {
+    captions[i] = links[i].getAttribute(`data-caption`);
 
-        if (!captions[i].toLowerCase().includes(input)) {
-            links[i].style.display=`none`;  
-        }
-        else {
-            links[i].style.display=``;               
-        }
+    if (!captions[i].toLowerCase().includes(input)) {
+      links[i].style.display=`none`;  
     }
+    else {
+      links[i].style.display=``;         
+    }
+  }
 };
